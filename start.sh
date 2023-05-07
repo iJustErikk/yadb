d801f52cebf74cef4c0fb2207711182ca99aaccd
Erik Awwad <awadoferik@gmail.com>
2023-05-07 01:28:44 +0000

    ahahah why is librocksdb-sys not installing

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-docker build -t yadb .
-docker run --rm -it -v $(pwd)/data:/usr/src/app/yadb yadb
+if which cargo > /dev/null; then
+  echo "bash is annoying, ignore this"
+else
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
+fi
+sudo apt install build-essential
+sudo apt install -y clang
+sudo apt-get install libclang-dev
+rustup toolchain install nightly
+cargo +nightly install -Z sparse-registry --debug cargo-ament-build 
+cargo build
+cargo run
