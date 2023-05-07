49e07ef62e59f87bf92be764d104fb85e707b95b
Erik Awwad <awadoferik@gmail.com>
2023-05-07 16:28:49 +0000

    basic tcp interface, need to figure out how to build client

---
+++ src/client.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+
+fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:8000").unwrap();
+    let mut buffer = [0; 1024];
+
+
+    loop {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        stream.write_all(command.as_bytes()).unwrap();
+
+        let bytes_read = stream.read(&mut buffer).unwrap();
+        // https://stackoverflow.com/questions/19076719/how-do-i-convert-a-vector-of-bytes-u8-to-a-string
+        let response = String::from_utf8_lossy(&buffer[..bytes_read]);
+        print!("{}", response);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,85 @@
-
-extern crate rocksdb;
 use rocksdb::{DB, Options};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream, db: &DB) {
+    loop {
+        match read_client_command(&mut stream) {
+            Ok(command) => {
+                if command.is_empty() {
+                    continue;
+                }
+                // if process_command (the database) fails
+                // just fail
+                // figure out a better solution later
+                let response = process_command(&command, db);
+                if let Err(e) = send_response(&mut stream, &response) {
+                    eprintln!("Network: Request: {}", e);
+                }
+            }
+            Err(e) => {
+                // eprintln!("Network: Other network error: {} {}", e.kind(), e);
+                eprintln!("Network: Response: {}", e);
+                break;
+            }
+        }
+    }
+}
+
+fn read_client_command(stream: &mut TcpStream) -> Result<Vec<String>, IoError> {
+    let mut buffer = [0; 1024];
+    // propogate err the rustonic way
+    let bytes_read = stream.read(&mut buffer)?;
+    // convert invalid utf8 chars to unknown chars
+    // need to figure out why i need to_owned for this to compile
+    let command: Vec<String> = String::from_utf8_lossy(&buffer[..bytes_read])
+        .split_whitespace()
+        .map(|s| s.to_owned())
+        .collect();
+    Ok(command)
+}
+
+fn process_command(command: &[String], db: &DB) -> String {
+    match command.get(0).map(String::as_str) {
+        Some("put") if command.len() == 3 => {
+            let key = command[1].as_bytes();
+            let value = command[2].as_bytes();
+            db.put(key, value).unwrap();
+            "Success\n".to_string()
+        }
+        Some("get") if command.len() == 2 => {
+            let key = command[1].as_bytes();
+            match db.get(key).unwrap() {
+                Some(value) => String::from_utf8(value.to_vec()).unwrap() + "\n",
+                None => "Key not found\n".to_string(),
+            }
+        }
+        // Some("delete") if command.len() == 2 => {
+        //     let key = command[1].as_bytes();
+        //     db.delete(key).unwrap();
+        //     "Success\n".to_string()
+        // }
+        _ => "Invalid command\n".to_string(),
+    }
+}
+
+fn send_response(stream: &mut TcpStream, response: &str) -> Result<(), IoError> {
+    stream.write_all(response.as_bytes())
+}
+
+
+
 
 fn main() {
-    let mut opts = Options::default();
-    opts.create_if_missing(true);
+    let path = "yadb";
+    let db = DB::open_default(path).unwrap();
 
-    
-    let db = DB::open(&opts, "yadb").unwrap();
-    db.put(b"key", b"value").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    println!("server up");
 
-    
-    match db.get(b"key") {
-        Ok(Some(value)) => {
-            let value_str = String::from_utf8(value.to_vec()).unwrap();
-            println!("Stored value: {}", value_str);
-        }
-        Ok(None) => println!("No value found for the given key."),
-        Err(e) => println!("Error reading from the database: {}", e),
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("client at: {}", stream.peer_addr().unwrap());
+        handle_client(stream, &db);
     }
-
-    db.delete(b"key").unwrap();
 }
